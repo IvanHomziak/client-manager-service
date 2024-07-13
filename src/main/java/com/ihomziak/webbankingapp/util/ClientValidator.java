608deea75baff8c2c9d2c80c98b30f5bdf16c58e
ivanhomziak <ivan.homziak@gmail.com>
2024-07-13 22:54:41 +0200

    commented ClientValidation.class

--- src/main/java/com/ihomziak/webbankingapp/util/ClientValidator.java
+++ src/main/java/com/ihomziak/webbankingapp/util/ClientValidator.java
@@ -7,30 +7,32 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
-@Component
-public class ClientValidator implements Validator {
-
-    private final ClientServiceImpl clientService;
-
-    @Autowired
-    public ClientValidator(ClientServiceImpl clientService) {
-        this.clientService = clientService;
-    }
-
-    @Override
-    public boolean supports(Class<?> aClass) {
-        return Client.class.equals(aClass);
-    }
-
-    @Override
-    public void validate(Object o, Errors errors) {
-        Client client = (Client) o;
-
-        if (client == null) {
-            return;
-        }
-
-        if (clientService.findClientByEmail(client).isEmpty())
-            errors.rejectValue("id" + client.getClientId(), "Is already exist");
-    }
+//@Component
+public class ClientValidator
+//        implements Validator
+{
+//
+//    private final ClientServiceImpl clientService;
+//
+////    @Autowired
+//    public ClientValidator(ClientServiceImpl clientService) {
+//        this.clientService = clientService;
+//    }
+//
+//    @Override
+//    public boolean supports(Class<?> aClass) {
+//        return Client.class.equals(aClass);
+//    }
+//
+//    @Override
+//    public void validate(Object o, Errors errors) {
+//        Client client = (Client) o;
+//
+//        if (client == null) {
+//            return;
+//        }
+//
+////        if (clientService.findClientByEmail(client).isEmpty())
+////            errors.rejectValue("id" + client.getClientId(), "Is already exist");
+//    }
 }
