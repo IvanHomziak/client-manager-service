95710e2785d0bf73259f0e67d09bdb62992dd68a
ivanhomziak <ivan.homziak@gmail.com>
2024-07-13 23:45:05 +0200

    added mapper clientToClientResponseDTO, clientsToClientInfoDTO

--- src/main/java/com/ihomziak/webbankingapp/mapper/MapStructMapper.java
+++ src/main/java/com/ihomziak/webbankingapp/mapper/MapStructMapper.java
@@ -1,9 +1,20 @@
 package com.ihomziak.webbankingapp.mapper;
 
 import com.ihomziak.webbankingapp.dto.ClientRequestDTO;
+import com.ihomziak.webbankingapp.dto.ClientResponseDTO;
+import com.ihomziak.webbankingapp.dto.ClientsInfoDTO;
 import com.ihomziak.webbankingapp.entity.Client;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
 
 public interface MapStructMapper {
 
     Client clientRequestDTOToClient(ClientRequestDTO clientRequestDTO);
+
+    ClientResponseDTO clientToClientResponseDTO(Optional<Client> theClient);
+
+    LinkedList<ClientsInfoDTO> clientsToClientInfoDTO(List<Client> clients);
 }
+
--- src/main/java/com/ihomziak/webbankingapp/mapper/MapStructMapperImpl.java
+++ src/main/java/com/ihomziak/webbankingapp/mapper/MapStructMapperImpl.java
@@ -1,8 +1,15 @@
 package com.ihomziak.webbankingapp.mapper;
 
 import com.ihomziak.webbankingapp.dto.ClientRequestDTO;
+import com.ihomziak.webbankingapp.dto.ClientResponseDTO;
+import com.ihomziak.webbankingapp.dto.ClientsInfoDTO;
 import com.ihomziak.webbankingapp.entity.Client;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public class MapStructMapperImpl implements MapStructMapper {
@@ -25,5 +32,44 @@ public class MapStructMapperImpl implements MapStructMapper {
         client.setDateOfBirth(clientRequestDTO.getDateOfBirth());
 
         return client;
+    }
+
+    @Override
+    public ClientResponseDTO clientToClientResponseDTO(Optional<Client> theClient) {
+        Client client = theClient.get();
+        ClientResponseDTO responseDTO = new ClientResponseDTO();
+
+        responseDTO.setFirstName(client.getFirstName());
+        responseDTO.setLastName(client.getLastName());
+        responseDTO.setTaxNumber(client.getTaxNumber());
+        responseDTO.setAddress(client.getAddress());
+        responseDTO.setEmail(client.getEmail());
+        responseDTO.setPhoneNumber(client.getPhoneNumber());
+        responseDTO.setDateOfBirth(client.getDateOfBirth());
+        responseDTO.setUUID(client.getUUID());
+        responseDTO.setCreatedAt(client.getCreatedAt());
+        responseDTO.setUpdateAt(client.getUpdatedAt());
+
+        return responseDTO;
+    }
+
+    @Override
+    public LinkedList<ClientsInfoDTO> clientsToClientInfoDTO(List<Client> clients) {
+
+        ClientsInfoDTO clientInfoDTO = new ClientsInfoDTO();
+        LinkedList<ClientsInfoDTO> clientsInfoDTOList = new LinkedList<>();
+
+        for (int i = 0; i < clients.size(); i++) {
+            Client client = clients.get(i);
+
+            clientInfoDTO.setFirstName(client.getFirstName());
+            clientInfoDTO.setLastName(client.getLastName());
+            clientInfoDTO.setEmail(client.getEmail());
+            clientInfoDTO.setPhoneNumber(client.getPhoneNumber());
+
+            clientsInfoDTOList.add(clientInfoDTO);
+        }
+
+        return clientsInfoDTOList;
     }
 }
