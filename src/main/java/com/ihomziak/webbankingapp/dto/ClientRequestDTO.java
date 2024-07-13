a2a4c9280a39da0aaa17c0c50e0a44127a58065b
ivanhomziak <ivan.homziak@gmail.com>
2024-07-13 22:48:08 +0200

    added ClientRequestDTO

--- src/main/java/com/ihomziak/webbankingapp/dto/ClientRequestDTO.java
+++ src/main/java/com/ihomziak/webbankingapp/dto/ClientRequestDTO.java
@@ -12,9 +12,8 @@ public class ClientRequestDTO {
     private String firstName;
     private String lastName;
     private String dateOfBirth;
+    private String taxNumber;
     private String email;
     private String phoneNumber;
     private String address;
-    private Timestamp createdAt;
-    private Timestamp updateUt;
 }
