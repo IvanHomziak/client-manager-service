82ec5e3b6ac2a65f3a8a8049ced735216ce8c054
ivanhomziak <ivan.homziak@gmail.com>
2024-08-11 14:57:38 -0400

    added new     List<Account> findAccountsByAccountType(AccountType accountType);

--- src/main/java/com/ihomziak/webbankingapp/dao/AccountRepository.java
+++ src/main/java/com/ihomziak/webbankingapp/dao/AccountRepository.java
@@ -1,9 +1,11 @@
 package com.ihomziak.webbankingapp.dao;
 
 import com.ihomziak.webbankingapp.entity.Account;
+import com.ihomziak.webbankingapp.enums.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +14,6 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findAccountByUUID(String uuid);
 
     Optional<Account> findAccountByAccountNumber(String accountNumber);
+
+    List<Account> findAccountsByAccountType(AccountType accountType);
 }
